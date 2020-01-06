94755cfa2cc9912c827564fc8090aadfc22af1b2
namlxag98 <namlxag98@gmail.com>
2020-01-06 11:13:56 +0700

    up test

---
+++ truffle-config.js
@@ -0,0 +1,12 @@
+// import * from "babel-register";
+require('@babel/register');
+module.exports = {
+  networks: {
+    ganache: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '*',
+      gas: 4700000
+    }
+  }
+}
